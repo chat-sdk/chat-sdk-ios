0cee19bf0d3bc65f7478228431d5ef19492f64ff
simonsmiley <simon_smiley@hotmail.com>
2017-09-20 12:40:30 +1000

    updated the audio manager - this included separating the playing and recording functions and adding better functions to allow playing at certain points and returning when an audio url has been successfully loaded

--- ChatSDK/ChatSDKCore/Classes/BAudioManager.h
+++ ChatSDK/ChatSDKCore/Classes/BAudioManager.h
@@ -9,6 +9,8 @@
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
 
+#import <RXPromise/RXPromise.h>
+
 // TODO: Refactor this - on audio branch
 #define bStopAudioNotification @"stopAudio"
 
@@ -17,22 +19,41 @@
     AVAudioRecorder * _recorder;
     AVPlayer * _player;
     
-    NSString * _currentAudioURL;
+    // This promise allows us to see when a URL is ready to play - we can return it in one of the avplayers delegate methods
+    RXPromise * _loadingPromise;
 }
+
+// This allows us to compare the current URL with one we are trying to set (play, pause etc)
+@property (nonatomic, strong) NSURL * currentAudioURL;
 
 @property (nonatomic, readwrite) AVAudioRecorder * recorder;
 
 +(BAudioManager *) sharedManager;
 
-- (void)playAudioWithURL: (NSString *)audioURL;
+- (void)playAudio;
 - (void)pauseAudio;
 - (void)stopAudio;
 
+- (BOOL)isPlaying;
+
+// This allows us to play audio from a certain point in the track
+- (void)playAudioWithURL:(NSURL *)audioURL percent: (CGFloat)percent;
+- (void)playAudioWithURL:(NSURL *)audioURL;
+
+// This sets up the audio ready to play
+- (void)prepareToPlayWithURL: (NSString *)audioURL;
+
+// Get the current time for the current audio file
 - (NSInteger)getAudioTime;
 
-- (void) startRecording;
-- (void) finishRecording;
+- (void)startRecording;
+- (void)finishRecording;
 
 -(double) recordingLength;
+
+- (void)setCurrentPlayTime: (double)percent;
+
+// This function gives us a call back when the audio is ready to be played
+- (RXPromise *)loadAudioFromURL: (NSURL *)audioURL;
 
 @end
--- ChatSDK/ChatSDKCore/Classes/BAudioManager.m
+++ ChatSDK/ChatSDKCore/Classes/BAudioManager.m
@@ -13,6 +13,7 @@
 static BAudioManager * manager;
 
 @synthesize recorder = _recorder;
+@synthesize currentAudioURL = _currentAudioURL;
 
 +(BAudioManager *) sharedManager {
     
@@ -29,40 +30,79 @@ static BAudioManager * manager;
 
 -(id) init {
     if ((self = [super init])) {
-        
-        // Set the audio file
-        NSArray * pathComponents = [NSArray arrayWithObjects:
-                                    [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
-                                    @"MyAudioMemo.m4a",
-                                    nil];
-        
-        NSURL * outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
-        
-        // Setup audio session
-        AVAudioSession * session = [AVAudioSession sharedInstance];
-        [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
-        
-        // Define the recorder setting
-        NSMutableDictionary * recordSetting = [[NSMutableDictionary alloc] init];
-        
-        [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
-        [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
-        [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
-        
-        // Initiate and prepare the recorder
-        _recorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:nil];
-        _recorder.delegate = self;
-        _recorder.meteringEnabled = YES;
-        [_recorder prepareToRecord];
-        
     }
     return self;
 }
 
+#pragma Playing audio
+
+- (void)playAudioWithURL:(NSURL *)audioURL percent: (CGFloat)percent {
+    
+    // Make sure we have an audio track and it has been loaded
+    if (_currentAudioURL == audioURL && CMTimeGetSeconds(_player.currentItem.duration)) {
+        
+        CGFloat currentPercent = CMTimeGetSeconds(_player.currentItem.currentTime)/CMTimeGetSeconds(_player.currentItem.duration);
+        
+        if (fabsf(percent - currentPercent) > 0.01) {
+            [self setCurrentPlayTime:percent];
+        }
+    }
+    else {
+        
+        _currentAudioURL = audioURL;
+        _player = [AVPlayer playerWithURL: _currentAudioURL];
+        [self setCurrentPlayTime:percent];
+    }
+    
+    _player.rate = 1.0;
+    [_player play];
+}
+
+// Load an audio track from a URL and then return the promise when it is ready to play
+- (RXPromise *)loadAudioFromURL: (NSURL *)audioURL {
+    
+    _loadingPromise = [RXPromise new];
+    
+    // Ensure we remove the observer when the _player is deallocated
+    @try {
+        [_player removeObserver:self forKeyPath:@"status"];
+    } @catch (NSException *exception) {
+    }
+    
+    _currentAudioURL = audioURL;
+    _player = [AVPlayer playerWithURL: _currentAudioURL];
+    [_player addObserver:self forKeyPath:@"status" options:0 context:nil];
+    
+    return _loadingPromise;
+}
+
+// This observer looks at whether the audio is ready to play then returns the loading promise
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    
+    if (object == _player && [keyPath isEqualToString:@"status"]) {
+        if (_player.status == AVPlayerStatusReadyToPlay) {
+            
+            NSInteger second = CMTimeGetSeconds(_player.currentItem.asset.duration);
+            
+            [_loadingPromise resolveWithResult:nil];
+        } else if (_player.status == AVPlayerStatusFailed) {
+            [_loadingPromise rejectWithReason:_player.error];
+        }
+    }
+}
+
+// Split the previous function into two, this allows us to prepare an audio track without necessarily playing it
 - (void)playAudioWithURL: (NSString *)audioURL {
     
-    // This means it has been paused and then played
-    if (![_currentAudioURL isEqualToString:audioURL]) {
+    [self prepareToPlayWithURL:audioURL];
+    [self playAudio];
+}
+
+// This prepares the app to play with a specific audio URL
+- (void)prepareToPlayWithURL: (NSString *)audioURL {
+    
+    // We don't need to prepare it if we are already on it
+    if (![_currentAudioURL.absoluteString isEqualToString:audioURL]) {
         
         _currentAudioURL = audioURL;
         
@@ -79,22 +119,18 @@ static BAudioManager * manager;
         // Subscribe to the AVPlayerItem's DidPlayToEndTime notification.
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(itemDidFinishPlaying:) name:AVPlayerItemDidPlayToEndTimeNotification object:playerItem];
     }
-    
-    [_player play];
 }
 
-// Check if the user has their headphones plugged in
-- (BOOL)isHeadsetPluggedIn {
-    AVAudioSessionRouteDescription* route = [[AVAudioSession sharedInstance] currentRoute];
-    for (AVAudioSessionPortDescription* desc in [route outputs]) {
-        if ([[desc portType] isEqualToString:AVAudioSessionPortHeadphones])
-            return YES;
-    }
-    return NO;
+- (void)playAudio {
+    [_player play];
 }
 
 - (void)pauseAudio {
     [_player pause];
+}
+
+- (BOOL)isPlaying {
+    return _player.rate != 0 && _player.error == nil;
 }
 
 - (void)stopAudio {
@@ -112,7 +148,60 @@ static BAudioManager * manager;
     [self stopAudio];
 }
 
+// This allows us to set the play time of the current audio track
+- (void)setCurrentPlayTime: (double)percent {
+    
+    // If we don't have one we create one with our current audio URL
+    if (_player.currentItem == nil) {
+        
+        AVPlayerItem * playerItem = [AVPlayerItem playerItemWithURL:_currentAudioURL];
+        _player = [AVPlayer playerWithPlayerItem:playerItem];
+        _player.rate = 1.0;
+    }
+    
+    CGFloat totalTime = CMTimeGetSeconds(_player.currentItem.duration);
+    
+    // CMTime deals in time increments, this means we can get more accurate by splitting it into 10ths of a seconds
+    // By using an increment of 100 instead of 1 we ensure that the miliseconds are preserved
+    CMTime newTime = CMTimeMakeWithSeconds(totalTime * percent, 1000);
+    
+    [_player.currentItem seekToTime:newTime toleranceBefore:kCMTimeZero toleranceAfter:kCMTimeZero];
+}
+
+#pragma Recording audio
+
+- (void)prepareToRecord {
+    
+    // Set the audio file
+    NSArray * pathComponents = [NSArray arrayWithObjects:
+                                [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                                @"MyAudioMemo.m4a",
+                                nil];
+    
+    NSURL * outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+    
+    // Setup audio session
+    AVAudioSession * session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    
+    // Define the recorder setting
+    NSMutableDictionary * recordSetting = [[NSMutableDictionary alloc] init];
+    
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+    
+    // Initiate and prepare the recorder
+    _recorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:nil];
+    _recorder.delegate = self;
+    _recorder.meteringEnabled = YES;
+    [_recorder prepareToRecord];
+}
+
 - (void)startRecording {
+    
+    // We can prepare the recording immediately before we record
+    [self prepareToRecord];
     
     // Make sure the recorder isn't currently recording
     if (!_recorder.recording) {
@@ -133,6 +222,7 @@ static BAudioManager * manager;
     [audioSession setActive:NO error:nil];
 }
 
+// This is very slow - might be better to look at the current asset if we can?
 -(double) recordingLength {
     AVURLAsset * asset = [[AVURLAsset alloc] initWithURL:_recorder.url options:nil];
     CMTime time = asset.duration;
@@ -140,9 +230,20 @@ static BAudioManager * manager;
 }
 
 - (NSInteger)getAudioTime {
-    AVPlayerItem * currentItem = _player.currentItem;
-    CMTime currentTime = currentItem.currentTime;
+    CMTime currentTime = _player.currentItem.currentTime;
     return CMTimeGetSeconds(currentTime);
+}
+
+#pragma Other functions
+
+// Check if the user has their headphones plugged in
+- (BOOL)isHeadsetPluggedIn {
+    AVAudioSessionRouteDescription* route = [[AVAudioSession sharedInstance] currentRoute];
+    for (AVAudioSessionPortDescription* desc in [route outputs]) {
+        if ([[desc portType] isEqualToString:AVAudioSessionPortHeadphones])
+            return YES;
+    }
+    return NO;
 }
 
 @end
