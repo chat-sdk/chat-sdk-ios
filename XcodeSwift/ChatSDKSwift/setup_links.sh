e70ad48466e43f08272eb23fb282e59b747128be
Ben Smiley-Andrews <ben@chatsdk.co>
2017-10-17 02:04:41 +0200

    Added setup links file

---
+++ XcodeSwift/ChatSDKSwift/setup_links.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Enter the path to the ChatSDK folder or leave blank to use the default path (../../)"
+read path
+
+adapterDir="ChatSDKFirebaseAdapter"
+chatDir="ChatSDK"
+firebaseModulesDir="ChatSDKFirebase"
+modulesDir="ChatSDKModules"
+
+adapterPath="$chatDir/$adapterDir/Classes"
+
+rm $adapterDir
+
+if [$path -eq ""] 
+then
+  path="../.."
+fi
+chatPath="$path/$adapterPath"
+firebaseModulesPath="$path/$firebaseModulesDir"
+modulesPath="$path/$modulesDir"
+
+echo "Creating simlink to $chatPath"
+ln -s "$chatPath" ChatSDKFirebaseAdapter
+
+# Create a simlink to the Firebase modules path
+echo "Creating simlink to $firebaseModulesPath"
+ln -s "$firebaseModulesPath" ChatSDKFirebase
+
+# Create a simlink to the modules path
+echo "Creating simlink to $modulesPath"
+ln -s "$modulesPath" ChatSDKModules
