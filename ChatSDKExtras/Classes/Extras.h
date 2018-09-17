4f9eab618078772c99b17249aee85af413fc95a9
Ben Smiley-Andrews <ben@chatsdk.co>
2018-09-17 12:47:46 +0100

    Small update to deploy to Cocoapods

---
+++ ChatSDKExtras/Classes/Extras.h
@@ -0,0 +1,13 @@
+//
+//  Extras.h
+//  ChatSDK Demo
+//
+//  Created by Ben on 9/17/18.
+//  Copyright © 2018 deluge. All rights reserved.
+//
+
+#ifndef Extras_h
+#define Extras_h
+
+
+#endif /* Extras_h */
