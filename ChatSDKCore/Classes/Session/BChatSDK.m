f8b53c4c654891ba1f12033ca0137f19da11102d
Ben Smiley <bensmiley45@hotmail.com>
2018-11-22 13:16:04 +0100

    Update BChatSDK.m

--- ChatSDKCore/Classes/Session/BChatSDK.m
+++ ChatSDKCore/Classes/Session/BChatSDK.m
@@ -109,9 +109,7 @@ static BChatSDK * instance;
 +(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [application setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
     
-    BOOL needToAskNotificationPermission = BChatSDK.config.askNotificationsPermission;
-    
-    if(BChatSDK.push && needToAskNotificationPermission) {
+    if(BChatSDK.push && BChatSDK.config.askForNotificationsPermission) {
         [BChatSDK.push registerForPushNotificationsWithApplication:application launchOptions:launchOptions];
     }
     if(BChatSDK.socialLogin) {
