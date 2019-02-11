7e6b745de8f8ad0dfbab199229306bf7495aff88
Ben Smiley <bensmiley45@hotmail.com>
2019-02-11 11:36:41 +0800

    Update root path

--- Xcode/ChatSDK Demo/AppDelegate.m
+++ Xcode/ChatSDK Demo/AppDelegate.m
@@ -22,7 +22,7 @@
     // The network adapter handles network traffic
 
     BConfiguration * config = [BConfiguration configuration];
-    config.rootPath = @"18_12_refactor";
+    config.rootPath = @"19_02";
     config.allowUsersToCreatePublicChats = NO;
     config.showEmptyChats = NO;
     config.googleMapsApiKey = @"AIzaSyCwwtZrlY9Rl8paM0R6iDNBEit_iexQ1aE";
