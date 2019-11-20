ce7a0b95e1edfbae9c1f43549653c9be7d642dd2
Dennis Li <denniszxxc@gmail.com>
2019-11-20 14:53:43 +0800

    Fix the warnings "method conflicts with same method from another" on Xcode 11+ (issue #408)
    class `@implementation` in .h header file are moved to .m files for (UIImage+Additions.h & UIImage+ImageEffect.h)

---
+++ ChatSDKUI/Classes/Categories/UIImage+Additions.m
@@ -0,0 +1,52 @@
+#import "UIImage+Additions.h"
+
+@implementation UIImage(Additions)
+
+-(NSString *) saveImageToFileWithName: (NSString *) name withFolder: (NSString *) folder {
+    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+
+	// If you go to the folder below, you will find those pictures
+
+    NSString * fileName = [NSString stringWithFormat:@"%@.png", name];
+
+	NSString *pngFolderPath = [docDir stringByAppendingPathComponent:folder];
+    NSString *pngFilePath = [pngFolderPath stringByAppendingPathComponent:fileName];
+
+    // Check if the folder exists
+    if (![[NSFileManager defaultManager] fileExistsAtPath:pngFolderPath])
+        [[NSFileManager defaultManager] createDirectoryAtPath:pngFolderPath withIntermediateDirectories:NO attributes:nil error:Nil];
+
+    // Delete the image if it exists already
+    if ([[NSFileManager defaultManager] fileExistsAtPath:pngFilePath])
+        [[NSFileManager defaultManager] removeItemAtPath:pngFilePath error:Nil];
+
+	NSLog(@"Saving png: %@ ...", name);
+	NSData *data1 = [NSData dataWithData:UIImagePNGRepresentation(self)];
+	[data1 writeToFile:pngFilePath atomically:YES];
+
+    return pngFilePath;
+}
+
++(UIImage *) getImageForPath: (NSString *) path {
+    return [UIImage imageWithContentsOfFile:path];
+}
+
++(UIImage *) getImageFromFile: (NSString *) name withFolder: (NSString *) folder {
+    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString * fileName = [NSString stringWithFormat:@"%@.png", name];
+
+	NSString *pngFolderPath = [docDir stringByAppendingPathComponent:folder];
+    NSString *pngFilePath = [pngFolderPath stringByAppendingPathComponent:fileName];
+	NSLog(@"Loading png: %@ ...", name);
+    return [self getImageForPath:pngFilePath];
+}
+
++(NSString *) getPathForFile: (NSString *) name withFolder: (NSString *) folder {
+    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString * fileName = [NSString stringWithFormat:@"%@.png", name];
+
+	NSString *pngFolderPath = [docDir stringByAppendingPathComponent:folder];
+    return [pngFolderPath stringByAppendingPathComponent:fileName];
+}
+
+@end
