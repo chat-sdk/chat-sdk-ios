bd9bc4fa1a6a893eb342c63ded0729d1e92c349b
Ben Smiley <bensmiley45@hotmail.com>
2020-04-07 12:31:53 +0200

    Added example demo view controller

---
+++ Xcode/ChatSDK Demo/Customization/DemoLoginViewController.m
@@ -0,0 +1,33 @@
+//
+//  DemoLoginViewController.m
+//  ChatSDK Demo
+//
+//  Created by ben3 on 06/04/2020.
+//  Copyright © 2020 deluge. All rights reserved.
+//
+
+
+#import "DemoLoginViewController.h"
+
+@interface DemoLoginViewController ()
+
+@end
+
+@implementation DemoLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
